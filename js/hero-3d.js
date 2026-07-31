da6544c48db1aac501bac0cd555d32f279880858
Senat-MarliGroup <tursunovigorexa103@gmail.com>
2026-07-31 18:07:37 +0300

    Update hero-3d.js

--- js/hero-3d.js
+++ js/hero-3d.js
@@ -1,8 +1,6 @@
 (function(){
-  // Hero video autoplay fix for mobile browsers
   const heroVideo = document.querySelector('.hero-video');
   if(heroVideo){
     heroVideo.play().catch(()=>{});
   }
 })();
-
